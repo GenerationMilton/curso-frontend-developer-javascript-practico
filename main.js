3c4ffa6999cf4db245abfa73bdf8afff628cb5d7
Milton Muñoz <livemilton@gmail.com>
2024-07-24 06:51:04 -0500

    list of products - HTML incoming arrays

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const menuHamIcon= document.querySelector('.menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const mobileMenu=document.querySelector('.mobile-menu');
 const aside = document.querySelector('.product-detail');
+const cardsContainer= document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 
@@ -48,3 +49,70 @@ function toggleCarritoAside(){
     aside.classList.toggle('inactive');
     
 }
+
+
+//funcion products
+const productList =[];
+productList.push({
+    name: 'Bike',
+    price:120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+    name: 'Pantalla',
+    price:220,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+    name: 'Compu',
+    price:620,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+
+function renderProducts(arr){
+    for(product of arr){
+
+        //CREAR LA MAQUETACION DEL HTML
+        const productCard= document.createElement('div');
+        productCard.classList.add('product-card');
+    
+        //product ={name,price,image}-> product.image
+        const productImg = document.createElement('img'); //Crea el elemento imagen
+        productImg.setAttribute('src', product.image); // Se le agrega el atributo a la imagen con la lista de productos del array que se hizo arriba
+    
+        const productInfo = document.createElement('div'); // Crea el contenedor div
+        productInfo.classList.add('product-info'); // le añade la clase product-info al productInfo(div)
+    
+        const productInfoDiv = document.createElement('div');//Crea el contenedor div vacio
+    
+        const productPrice= document.createElement('p');//Crea el parrafo <p>
+        productPrice.innerText= '$' + product.price;//le añade el precio al parrafo productPrice con la lista de productos del array que se hizo
+        
+        const productName = document.createElement('p');//Crea el parrafo <p>
+        productName.innerText = product.name;//le añade el nombre al parrafo productName con la lista de productos del array que se hizo arriba
+    
+        const productInfoFigure= document.createElement('figure'); //Crea el figure<figure>
+        const productImgCart=document.createElement('img');//Crea el elemento imagen
+        productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');//le añade el atributo de la imagen a productImgCart
+
+        //APPENCHILD LA MAQUETACION Al HTML DE ARRIBA HACIA ABAJO 
+        cardsContainer.appendChild(productCard); //arriba se selecciono la clase (cards-container)estamos apendiando el productCard
+
+        productCard.appendChild(productImg);//Se le hizo append el productImg al productCard
+        productCard.appendChild(productInfo);//se le hizo append el productInfo al productCard
+
+        productInfoDiv.appendChild(productPrice);//se le hizo append el productPrice al productInfoDiv
+        productInfoDiv.appendChild(productName);//se le hizo append el productName al productInfoDiv
+
+        productInfo.appendChild(productInfoDiv);//se le hizo apppend el productInfoDiv al productInfo
+        productInfo.appendChild(productInfoFigure);//se le hizo apppend el productInfoFigure al productInfo
+        productInfoFigure.appendChild(productImgCart);//se le hizo append el productImgCart al productInfoFigure
+       
+    
+        
+    
+        
+    }
+}
+
+renderProducts(productList);
