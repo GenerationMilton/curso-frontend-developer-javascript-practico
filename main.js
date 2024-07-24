c34b0374f898dc9204621ee1cc9c3fe016a85ca2
Milton Muñoz <livemilton@gmail.com>
2024-07-23 23:10:31 -0500

    car shopping build

--- main.js
+++ main.js
@@ -1,18 +1,50 @@
 const menuEmail= document.querySelector('.navbar-email');
 const desktopMenu= document.querySelector('.desktop-menu');
 const menuHamIcon= document.querySelector('.menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const mobileMenu=document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 
 menuHamIcon.addEventListener('click',toggleMobileMenu);
 
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
+
+//fucntion 1
 function toggleDesktopMenu(){
     console.log('Click');
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if(!isAsideClosed){
+        aside.classList.add('inactive');
+    }
+
     desktopMenu.classList.toggle('inactive');
 }
 
+//fucntion 2
 function toggleMobileMenu(){
     console.log('Click');
+
+    const isAsideClosed= aside.classList.contains('inactive');
+
+    if(!isAsideClosed){
+        aside.classList.add('inactive');
+    }
+
     mobileMenu.classList.toggle('inactive');
+}
+
+//fucntion 3
+function toggleCarritoAside(){
+    console.log('click');
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    
+    if(!isMobileMenuClosed){
+        mobileMenu.classList.add('inactive');
+    }
+
+    aside.classList.toggle('inactive');
+    
 }
